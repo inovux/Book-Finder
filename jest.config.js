019620e07f99c6fed2edf6b42c3f6f7cfb70a8b6
Stanley Thijssen <stanley.thijssen@gmail.com>
2020-10-14 22:33:48 +0800

    fix: Add css modules test mock and remove scripts from tests

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  moduleNameMapper: {
+    '\\.module.css$': 'identity-obj-proxy',
+  },
+  testPathIgnorePatterns: ['/scripts/'],
 }
