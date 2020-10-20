697ccbcaada865ac6737c3fe6458a01d378b29ed
Stanley Thijssen <stanley.thijssen@gmail.com>
2020-10-20 17:07:13 +0800

    feat: Add basic app structure and requirements

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React, { FC } from 'react'
 import { Button } from './components/Button'
+import { Input } from './components/Input'
 
 export const App: FC = () => {
   return (
     <div className="App">
-      <Button onClick={() => console.log('button clicked')}>LuL</Button>
+      <Input placeholder="Search books" />
+      <Button onClick={() => console.log('button clicked')}>Search</Button>
+      <h1>list of books:</h1>
+      <ul>
+        <li>BookCard 1</li>
+        <li>BookCard 2</li>
+        <li>BookCard 3</li>
+        <li>BookCard 4</li>
+        <li>BookCard 5</li>
+      </ul>
     </div>
   )
 }
