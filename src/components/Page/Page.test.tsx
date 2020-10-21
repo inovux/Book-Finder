872ababa1390a5e76d5f2a3a8fb89da0cc346919
Stanley Thijssen <stanley.thijssen@gmail.com>
2020-10-21 10:58:08 +0800

    test: Add test for Page component

---
+++ src/components/Page/Page.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import { Page } from './index'
+
+test('page should render with title', () => {
+  const { getByText, debug } = render(<Page title="Page Title" />)
+
+  const page = getByText(/page title/i)
+
+  expect(page).toBeInTheDocument()
+})
