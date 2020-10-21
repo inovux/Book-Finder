e60775dbcf723ed88b300e4655844ee42ca94808
Stanley Thijssen <stanley.thijssen@gmail.com>
2020-10-21 10:51:32 +0800

    feat: Add title prop to Page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React, { FC } from 'react'
-import { Button } from './components/Button'
-import { Input } from './components/Input'
-import { Page } from './components/Page'
+import { Button } from './components'
+import { Input } from './components'
+import { Page } from './components'
 
 export const App: FC = () => {
   return (
-    <Page>
-      <h1>Book Finder</h1>
+    <Page title="Book Finder">
       <Input placeholder="Search books" />
       <Button onClick={() => console.log('button clicked')}>Search</Button>
       <h1>list of books:</h1>
--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -2,9 +2,14 @@ import React, { FC } from 'react'
 
 import styles from './Page.module.css'
 
-export const Page: FC = ({ children }) => {
+interface IPage {
+  title: string
+}
+
+export const Page: FC<IPage> = ({ children, title }) => {
   return (
     <div className={styles.container}>
+      <h1 className={styles.title}>{title}</h1>
       <div className={styles.content}>{children}</div>
     </div>
   )
