05d98eb39d5ec10c29b7f1f306c4d41bcde37c4a
Stanley Thijssen <stanley.thijssen@gmail.com>
2020-10-23 21:49:29 +0800

    feat: Add loader component

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import styles from './Loader.module.css'
+
+export const Loader = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.spinner}>
+        <div />
+        <div />
+        <div />
+        <div />
+      </div>
+    </div>
+  )
+}
